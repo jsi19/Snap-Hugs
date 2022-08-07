931a0e719e30988ffe56d5c2fa9ae43966c255d3
vmalabanan <vanessamalabanan@yahoo.com>
2022-08-07 14:57:34 -0700

    style send to page

--- src/screens/SendToOverlay.js
+++ src/screens/SendToOverlay.js
@@ -11,10 +11,16 @@ import {
   VStack,
   Image,
 } from "native-base";
+
 import carrot from "../../assets/images/send-to-nav-bar/carrot.png";
 import magnifyingGlass from "../../assets/images/send-to-nav-bar/magnifying_glass.png";
 import peopleIcon from "../../assets/images/send-to-nav-bar/people_icon.png";
 import plus from "../../assets/images/send-to-nav-bar/plus.png";
+import ashley from "../../assets/send-to/ashley.png";
+import bernard from "../../assets/send-to/bernard.png";
+import bitmoji from "../../assets/send-to/bitmoji.png";
+import location from "../../assets/send-to/location.png";
+import shelby from "../../assets/send-to/shelby.png";
 
 export default function SendToOverlay(props) {
   // Set modal visibility
@@ -26,10 +32,10 @@ export default function SendToOverlay(props) {
 
   return (
     <GestureRecognizer style={{ flex: 1 }} onSwipeDown={hideModal}>
-      <Pressable
+      <TouchableOpacity
         style={styles.sendToButton}
         onPress={() => setModalVisible(true)}
-      ></Pressable>
+      ></TouchableOpacity>
 
       <Modal
         isVisible={isModalVisible}
@@ -79,123 +85,397 @@ export default function SendToOverlay(props) {
           </View>
 
           {/* Stories section */}
-          {/* Stories header */}
-          <HStack space={2} style={styles.storiesTop}>
-            <Text style={styles.stories} fontSize="xs" bold>
-              Stories
-            </Text>
-            <TouchableOpacity>
-              <Center
-                style={styles.newStoryButton}
-                bg="light.200"
-                rounded="3xl"
-              >
-                <HStack>
-                  <Image source={plus} alt="plus" style={styles.plus}></Image>
-                  <Text style={styles.newStory} fontSize="xs" bold>
-                    New Story
+          {/* Stories title */}
+          <HStack>
+            <View style={styles.Left}>
+              <HStack space={2} style={styles.storiesTop}>
+                <Text style={styles.stories} fontSize="xs" bold>
+                  Stories
+                </Text>
+                <TouchableOpacity>
+                  <Center
+                    style={styles.newStoryButton}
+                    bg="light.200"
+                    rounded="3xl"
+                  >
+                    {/* New Story button */}
+                    <HStack>
+                      <Image
+                        source={plus}
+                        alt="plus"
+                        style={styles.plus}
+                      ></Image>
+                      <Text style={styles.newStory} fontSize="xs" bold>
+                        New Story
+                      </Text>
+                    </HStack>
+                  </Center>
+                </TouchableOpacity>
+              </HStack>
+
+              {/* Stories boxes */}
+              <VStack style={styles.storiesBoxes}>
+                <TouchableOpacity>
+                  <Center h="50" w="365" bg="white" rounded="xl" shadow={2}>
+                    <HStack space={1}>
+                      <Image
+                        source={bitmoji}
+                        alt="bitmoji"
+                        style={styles.bitmoji}
+                      ></Image>
+                      <Text style={styles.buttonText} fontSize="md" bold>
+                        My Story
+                      </Text>
+                    </HStack>
+                  </Center>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Center h="50" w="365" bg="white" rounded="xl" shadow={2}>
+                    <HStack space={1}>
+                      <Image
+                        source={location}
+                        alt="location"
+                        style={styles.location}
+                      ></Image>
+                      <VStack>
+                        <Text style={styles.snapMapText} fontSize="md" bold>
+                          Snap Map
+                        </Text>
+                        <Text
+                          style={styles.snapMapSubtitleText}
+                          fontSize="xs"
+                          color="light.400"
+                        >
+                          Add your snap to the Map!
+                        </Text>
+                      </VStack>
+                    </HStack>
+                  </Center>
+                </TouchableOpacity>
+              </VStack>
+
+              {/* Post to hugs section */}
+              {/* Post to hugs header */}
+              <Text style={styles.stories} fontSize="xs" bold>
+                Post to Hugs
+              </Text>
+              {/* Hugs boxes */}
+              <VStack style={styles.storiesBoxes} space={1}>
+                <TouchableOpacity>
+                  <Center
+                    h="50"
+                    w="365"
+                    rounded="xl"
+                    shadow={2}
+                    style={styles.technology}
+                  >
+                    <Text
+                      style={styles.hugsText}
+                      fontSize="md"
+                      color="white"
+                      bold
+                    >
+                      Technology
+                    </Text>
+                  </Center>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Center
+                    h="50"
+                    w="365"
+                    rounded="xl"
+                    shadow={2}
+                    style={styles.engineering}
+                  >
+                    <Text
+                      style={styles.hugsText}
+                      fontSize="md"
+                      color="white"
+                      bold
+                    >
+                      Engineering
+                    </Text>
+                  </Center>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Center
+                    h="50"
+                    w="365"
+                    rounded="xl"
+                    shadow={2}
+                    style={styles.activism}
+                  >
+                    <Text
+                      style={styles.activismText}
+                      fontSize="md"
+                      color="white"
+                      bold
+                    >
+                      Activism
+                    </Text>
+                  </Center>
+                </TouchableOpacity>
+              </VStack>
+              <TouchableOpacity>
+                <Text
+                  style={styles.viewMore}
+                  fontSize="xs"
+                  bold
+                  color="light.400"
+                >
+                  View More
+                </Text>
+              </TouchableOpacity>
+
+              {/* Recents section */}
+              {/* Recents header */}
+              <Text style={styles.stories} fontSize="xs" bold>
+                Recents
+              </Text>
+
+              {/* Recents boxes */}
+              <VStack style={styles.storiesBoxes}>
+                <TouchableOpacity>
+                  <Center h="50" w="365" bg="white" rounded="xl" shadow={2}>
+                    <HStack space={1}>
+                      <Image
+                        source={bernard}
+                        alt="bernard"
+                        style={styles.recentsBitmoji}
+                      ></Image>
+                      <Text style={styles.recentsText} fontSize="md" bold>
+                        Bernard
+                      </Text>
+                    </HStack>
+                  </Center>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Center h="50" w="365" bg="white" rounded="xl" shadow={2}>
+                    <HStack space={1}>
+                      <Image
+                        source={ashley}
+                        alt="ashley"
+                        style={styles.ashleyBitmoji}
+                      ></Image>
+                      <Text style={styles.ashleyText} fontSize="md" bold>
+                        Ashley
+                      </Text>
+                    </HStack>
+                  </Center>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Center h="50" w="365" bg="white" rounded="xl" shadow={2}>
+                    <HStack space={1}>
+                      <Image
+                        source={shelby}
+                        alt="shelby"
+                        style={styles.ashleyBitmoji}
+                      ></Image>
+                      <Text style={styles.shelbyText} fontSize="md" bold>
+                        Shelby
+                      </Text>
+                    </HStack>
+                  </Center>
+                </TouchableOpacity>
+              </VStack>
+            </View>
+            <View style={styles.alphabetBar}>
+              <Center h="510" w="4" bg="light.200" rounded="xl">
+                <VStack>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    A
                   </Text>
-                </HStack>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    B
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    C
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    D
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    E
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    F
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    G
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    G
+                  </Text>
+
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    H
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    I
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    J
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    K
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    L
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    M
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    N
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    O
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    P
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    Q
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    R
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    S
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    T
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    U
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    V
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    W
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    X
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    Y
+                  </Text>
+                  <Text
+                    style={styles.alpabetText}
+                    fontSize="xs"
+                    color="light.400"
+                  >
+                    Z
+                  </Text>
+                </VStack>
               </Center>
-            </TouchableOpacity>
+            </View>
           </HStack>
-
-          {/* Stories boxes */}
-          <VStack style={styles.storiesBoxes}>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                bg="white"
-                rounded="xl"
-                shadow={2}
-              ></Center>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                bg="white"
-                rounded="xl"
-                shadow={2}
-              ></Center>
-            </TouchableOpacity>
-          </VStack>
-
-          {/* Post to hugs section */}
-          {/* Post to hugs header */}
-          <Text style={styles.stories} fontSize="xs" bold>
-            Post to Hugs
-          </Text>
-          {/* Stories boxes */}
-          <VStack style={styles.storiesBoxes}>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                rounded="xl"
-                shadow={2}
-                style={styles.technology}
-              ></Center>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                rounded="xl"
-                shadow={2}
-                style={styles.engineering}
-              ></Center>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                rounded="xl"
-                shadow={2}
-                style={styles.activism}
-              ></Center>
-            </TouchableOpacity>
-          </VStack>
-
-          {/* Recents section */}
-
-          {/* Recents header */}
-
-          <Text style={styles.stories} fontSize="xs" bold>
-            Recents
-          </Text>
-
-          {/* Recents boxes */}
-          <VStack style={styles.storiesBoxes}>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                bg="white"
-                rounded="xl"
-                shadow={2}
-              ></Center>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                bg="white"
-                rounded="xl"
-                shadow={2}
-              ></Center>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Center
-                h="50"
-                w="375"
-                bg="white"
-                rounded="xl"
-                shadow={2}
-              ></Center>
-            </TouchableOpacity>
-          </VStack>
         </View>
       </Modal>
     </GestureRecognizer>
@@ -207,10 +487,8 @@ const styles = StyleSheet.create({
     position: "absolute",
     width: 30,
     height: 30,
-
+    borderRadius: 40,
     marginTop: -30,
-    backgroundColor: "#fff",
-    opacity: 0,
   },
 
   modalView: {
@@ -258,7 +536,7 @@ const styles = StyleSheet.create({
   },
 
   stories: {
-    marginTop: 10,
+    marginTop: 20,
     marginLeft: 20,
   },
 
@@ -267,7 +545,7 @@ const styles = StyleSheet.create({
     paddingRight: 12,
     paddingTop: 3,
     paddingBottom: 3,
-    marginLeft: 225,
+    marginLeft: 210,
   },
 
   storiesTop: {
@@ -277,7 +555,75 @@ const styles = StyleSheet.create({
   storiesBoxes: {
     marginTop: 5,
     marginLeft: 20,
-    marginBottom: 5,
+    marginBottom: 10,
+  },
+
+  bitmoji: {
+    width: 40,
+    height: 40,
+    marginLeft: -180,
+    marginTop: -3,
+  },
+
+  buttonText: {
+    marginTop: 6,
+  },
+
+  location: {
+    width: 35,
+    height: 35,
+    marginLeft: -163,
+    marginTop: 2,
+  },
+
+  snapMapText: {
+    marginLeft: 3,
+    marginBottom: -3,
+  },
+
+  snapMapSubtitleText: {
+    marginLeft: 3,
+  },
+
+  hugsText: {
+    marginLeft: -258,
+  },
+
+  activismText: {
+    marginLeft: -280,
+  },
+
+  viewMore: {
+    marginLeft: 315,
+  },
+
+  recentsBitmoji: {
+    width: 40,
+    height: 40,
+    marginLeft: -183,
+    marginTop: -5,
+  },
+
+  recentsText: {
+    marginTop: 6,
+    marginLeft: -1,
+  },
+
+  ashleyBitmoji: {
+    width: 40,
+    height: 40,
+    marginLeft: -181,
+    marginTop: -5,
+  },
+
+  ashleyText: {
+    marginTop: 6,
+    marginLeft: -2,
+  },
+
+  shelbyText: {
+    marginTop: 6,
+    marginLeft: -2,
   },
 
   technology: {
@@ -290,5 +636,14 @@ const styles = StyleSheet.create({
 
   activism: {
     backgroundColor: "#000",
+  },
+
+  alphabetBar: {
+    marginTop: 120,
+    marginLeft: 9,
+  },
+
+  alpabetText: {
+    marginLeft: 3,
   },
 });
